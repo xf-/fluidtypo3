ee71d2a00770278995422e5db11b74424a5859dc
Claus Due <claus@namelesscoder.net>
2014-06-04 00:34:59 +0200

    [TASK] Add basic distribution files

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "fluidtypo3".
+ *
+ * Auto generated 25-03-2014 19:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FluidTYPO3 Distribution',
+	'description' => 'Preconfigured site contained in extension, installs a demonstration Fluid Powered TYPO3 site.',
+	'category' => 'example',
+	'author' => 'FluidTYPO3 Team',
+	'author_email' => 'claus@namelesscoder.net',
+	'author_company' => '',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '4.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
